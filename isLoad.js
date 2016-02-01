d1514dd2b3ddfb9384991f287bb7fd80234180f0
zzs0 <zzs0@zzs0.com>
2016-02-01 19:36:03 +0800

    version 1.0

---
+++ isLoad.js
@@ -0,0 +1,18 @@
+function checkScriptLoad() {
+  chrome.extension.onMessage.addListener(function(request, sender, response) {
+    if (request.msg == 'is_page_capturable') {
+      try {
+        if (isPageCapturable()) {
+          response({msg: 'capturable'});
+        } else {
+          response({msg: 'uncapturable'});
+        }
+      } catch(e) {
+        response({msg: 'loading'});
+      }
+    }
+    return true;
+  });
+}
+
+checkScriptLoad();
